cd21407519afeecbb8a45c783f7ebd5724826236
Daniel Azuma <dazuma@gmail.com>
2026-03-12 15:56:24 -0700

    chore: Update tools

--- .toys/.toys.rb
+++ .toys/.toys.rb
@@ -15,6 +15,4 @@ end
 
 expand :gem_build
 
-expand :gem_build, name: "release", push_gem: true
-
 expand :gem_build, name: "install", install_gem: true
--- .toys/release.rb
+++ .toys/release.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-load_gem "toys-release"
+load_gem "toys-release", version: ">= 0.8.1"
