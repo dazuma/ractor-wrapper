d90bbe8d0a7cb379a2b95bb42f40195e6b47b04d
Daniel Azuma <dazuma@gmail.com>
2026-03-28 21:32:37 -0700

    docs: Updates to README

--- test/test_example.rb
+++ test/test_example.rb
@@ -3,22 +3,30 @@
 require "helper"
 
 describe ::Ractor::Wrapper do
-  let(:ruby_block_finder) { /\n```ruby\n((?:(?:[^`][^\n]*)?\n)+)```\n/m }
+  let(:ruby_block_finder) { /\n```ruby\n#([^\n]+)\n((?:(?:[^`][^\n]*)?\n)+)```\n/m }
+  let(:readme_path) { ::File.join(::File.dirname(__dir__), "README.md") }
+  let(:readme_content) { ::File.read(readme_path) }
+
+  def eval_script(name, expr)
+    script = readme_content.scan(ruby_block_finder).find { |entry| entry[0].strip == name }&.[](1)
+    flunk("Unable to find example: #{name.inspect}") unless script
+    final_script = "#{script}\n#{expr}\n"
+    eval(final_script) # rubocop:disable Security/Eval
+  end
 
   it "runs the Net::HTTP README example" do
-    readme_content = ::File.read(::File.join(::File.dirname(__dir__), "README.md"))
-    script = readme_content.scan(ruby_block_finder)[0][0]
-    script = "#{script}\nresponse\n"
-    response = eval(script) # rubocop:disable Security/Eval
+    response = eval_script("Net::HTTP example", "response")
     assert_kind_of(Net::HTTPOK, response)
   end
 
   it "runs the SQLite3 README example" do
     $my_database_path = File.join(__dir__, "data", "numbers.db")
-    readme_content = ::File.read(::File.join(::File.dirname(__dir__), "README.md"))
-    script = readme_content.scan(ruby_block_finder)[1][0]
-    script = "#{script}\nrows\n"
-    rows = eval(script) # rubocop:disable Security/Eval
+    rows = eval_script("SQLite3 example", "rows")
     assert_equal([["one", 1], ["two", 2]], rows)
+  end
+
+  it "runs the actor example" do
+    sum = eval_script("Simple actor example", "sum")
+    assert_equal(5, sum)
   end
 end
